0468f146b95fb5037c43d157a70d9f5112ebf993
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-22 00:52:12 +0000

    Update indexer.go

--- indexer.go
+++ indexer.go
@@ -1,6 +1,4 @@
-
-		retu
-	df := float64(len(postings))
+ngs))
 	dl := float64(idx.DocLengths[docID])
 	idf := math.Log((N-df+0.5)/(df+0.5) + 1)
 	tfNorm := (tf * (k1 + 1)) / (tf + k1*(1-b+b*dl/idx.AvgDL))
