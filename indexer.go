ff865dd35687b70d3451a10cfbbb1ea28b1b99f3
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-22 00:39:54 +0000

    Update indexer.go

--- indexer.go
+++ indexer.go
@@ -6,9 +6,7 @@ func buildIndex(docsDir strin
 		InvertedIndex: make(map[string
 		Doc
 		DocN		if e.IsDir() || !strings.HasSuf
-		data, err := os.ReadFile(fil			co		idx.DocNames[docID] = e.Name()
-		idx.DocLengths[doc
-
+		data, err := os.ReadFile(fil			co		idx.DocNames[docI
 		freq := make(map[st			freq[t]++
 		}
 		for term, tf := range freq {
