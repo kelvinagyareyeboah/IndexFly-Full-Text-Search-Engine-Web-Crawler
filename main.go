13eac5b80db3dd61aed55fcb62a77b4fb2248d89
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-23 02:35:18 +0000

    Update main.go

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ xist(err) {
 	}
 
 	http.HandleFunc("/", handleSearch)
-	http.HandleFunc("/reindex", handleReindex)
-
-	fmt.Printf("\n  🔍  Search Engine running → http://localhost%s\n\n", port)
+	http.HandleFunc("/reindex"	fmt.Printf("\n  🔍  Search Engine running → http://localhost%s\n\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
