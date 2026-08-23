1a2435441e621c022acc94a4576313b5c2649309
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-23 02:27:12 +0000

    Update main.go

--- main.go
+++ main.go
@@ -7,8 +7,7 @@
 		path := filep
 		f, err 
 		scanner := bufi	if scanner.Scan() {
-			snippets[i	built, err := buildIndex(doc
-	}
+			snippets[i	built, err :=
 	if err := saveIndex(built, indexFile); err !
 	}
 	idx = built
