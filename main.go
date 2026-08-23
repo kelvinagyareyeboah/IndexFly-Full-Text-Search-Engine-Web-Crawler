665a19d4b411c8df32b4b40555255cea2dc272d6
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-23 02:34:01 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,7 @@
 func main() {
 	funcs := template.FuncMap{
 		"add": func(a, b int) int { return a + b },
-		"mul": func(a, b int) int { return a * b },
-	}
+		"mul": func(a, b int) int { ret
 	tmpl = temp
 	var err error
 	if _, err = os.Stat(indexFile); os.IsNotExist(err) {
